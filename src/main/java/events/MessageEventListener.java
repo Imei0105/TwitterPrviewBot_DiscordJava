edc7b8deddd56b02bb292ec9a1938b790c3cf96b
Imei <s83582003@gmail.com>
2024-01-15 13:33:36 +0800

    Bot v1.0

--- src/main/java/events/MessageEventListener.java
+++ src/main/java/events/MessageEventListener.java
@@ -2,39 +2,78 @@ package events;
 
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MessageEventListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         super.onMessageReceived(event);
 
-//        String twitterExist = "https://twitter\\.com/";
-//        Pattern twitterPattern = Pattern.compile(twitterExist);
-//        Matcher twitterMatcher = twitterPattern.matcher(""+event.getMessage());
 
         String twitterUrlCheck = "https://twitter.com/";
+        String xUrlCheck = "https://x.com/";
+        String redditUrlCheck = "https://www.reddit.com/";
+        String instagramCheck = "https://www.instagram.com/";
+        String tiktokCheck = "https://www.tiktok.com/";
 
-        if(event.getAuthor().isBot())
+
+        if (event.getAuthor().isBot())
             return;
-        else{
-            if(event.getMessage().getContentDisplay().equalsIgnoreCase("?help")){
+        else {
+            if (event.getMessage().getContentDisplay().equalsIgnoreCase("?help")) {
                 event.getChannel().sendMessage("Bot is on duty").queue();
-            }else if(event.getMessage().getContentDisplay().contains(twitterUrlCheck)){
-                StringBuilder sb = new StringBuilder(""+event.getMessage().getContentDisplay());
-                sb.insert(8,"vx");
+            } else if (event.getMessage().getContentDisplay().contains(twitterUrlCheck)) {
+                StringBuilder sb = new StringBuilder("" + event.getMessage().getContentDisplay());
+                sb.insert(8, "vx");
+                sb.insert(0, "[Preview]");
+                sb.insert(9, "(");
+                sb.append(")");
                 String twitterPreviewString = sb.toString();
 
                 event.getChannel().sendMessage(twitterPreviewString).queue();
-            }
+            } else if (event.getMessage().getContentDisplay().contains(xUrlCheck)) {
+                StringBuilder sb = new StringBuilder("" + event.getMessage().getContentDisplay());
+                sb.insert(8, "v");
+                sb.insert(10, "twitter");
+                sb.insert(0, "[Preview]");
+                sb.insert(9, "(");
+                sb.append(")");
+                String twitterPreviewString = sb.toString();
 
-        }
+                event.getChannel().sendMessage(twitterPreviewString).queue();
+            } else if (event.getMessage().getContentDisplay().contains(redditUrlCheck)) {
+                StringBuilder sb = new StringBuilder("" + event.getMessage().getContentDisplay());
+                sb.deleteCharAt(13);
+                sb.insert(13, "x");
+                sb.insert(0, "[Preview]");
+                sb.insert(9, "(");
+                sb.append(")");
+                String twitterPreviewString = sb.toString();
+
+                event.getChannel().sendMessage(twitterPreviewString).queue();
+            } else if (event.getMessage().getContentDisplay().contains(instagramCheck)) {
+                StringBuilder sb = new StringBuilder("" + event.getMessage().getContentDisplay());
+                sb.insert(21, "ez");
+                sb.insert(0, "[Preview]");
+                sb.insert(9, "(");
+                sb.append(")");
+                String twitterPreviewString = sb.toString();
+                event.getChannel().sendMessage(twitterPreviewString).queue();
+            } else if (event.getMessage().getContentDisplay().contains(tiktokCheck)) {
+                StringBuilder sb = new StringBuilder("" + event.getMessage().getContentDisplay());
+                sb.deleteCharAt(16);
+                sb.insert(16, "x");
+                sb.insert(0, "[Preview]");
+                sb.insert(9, "(");
+                sb.append(")");
+                String twitterPreviewString = sb.toString();
+
+                event.getChannel().sendMessage(twitterPreviewString).queue();
 
 //        System.out.println("User send: "+event.getMessage().getContentDisplay());
+            }
+        }
     }
 }
